eb17e8ac8f98352ba3bcb9033c2ab8ee164c5554
Syrenyx <paul@stagl.at>
2025-09-04 15:15:17 +0200

    Affliction Entity Effects

---
+++ src/main/java/syrenyx/distantmoons/affliction/effect/entity/SpawnParticlesEffect.java
@@ -0,0 +1,60 @@
+package syrenyx.distantmoons.affliction.effect.entity;
+
+import com.mojang.serialization.MapCodec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
+import net.minecraft.enchantment.effect.entity.SpawnParticlesEnchantmentEffect;
+import net.minecraft.entity.Entity;
+import net.minecraft.particle.ParticleEffect;
+import net.minecraft.particle.ParticleTypes;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.util.math.floatprovider.ConstantFloatProvider;
+import net.minecraft.util.math.floatprovider.FloatProvider;
+import net.minecraft.util.math.random.Random;
+import syrenyx.distantmoons.affliction.effect.AfflictionEntityEffect;
+
+public record SpawnParticlesEffect(
+    ParticleEffect particle,
+    SpawnParticlesEnchantmentEffect.PositionSource horizontalPosition,
+    SpawnParticlesEnchantmentEffect.PositionSource verticalPosition,
+    SpawnParticlesEnchantmentEffect.VelocitySource horizontalVelocity,
+    SpawnParticlesEnchantmentEffect.VelocitySource verticalVelocity,
+    FloatProvider speed
+) implements AfflictionEntityEffect {
+
+  public static final MapCodec<SpawnParticlesEffect> CODEC = RecordCodecBuilder.mapCodec(instance -> instance
+      .group(
+          ParticleTypes.TYPE_CODEC.fieldOf("particle").forGetter(SpawnParticlesEffect::particle),
+          SpawnParticlesEnchantmentEffect.PositionSource.CODEC.fieldOf("horizontal_position").forGetter(SpawnParticlesEffect::horizontalPosition),
+          SpawnParticlesEnchantmentEffect.PositionSource.CODEC.fieldOf("vertical_position").forGetter(SpawnParticlesEffect::verticalPosition),
+          SpawnParticlesEnchantmentEffect.VelocitySource.CODEC.fieldOf("horizontal_velocity").forGetter(SpawnParticlesEffect::horizontalVelocity),
+          SpawnParticlesEnchantmentEffect.VelocitySource.CODEC.fieldOf("vertical_velocity").forGetter(SpawnParticlesEffect::verticalVelocity),
+          FloatProvider.VALUE_CODEC.optionalFieldOf("speed", ConstantFloatProvider.ZERO).forGetter(SpawnParticlesEffect::speed)
+      )
+      .apply(instance, SpawnParticlesEffect::new)
+  );
+
+  @Override
+  public MapCodec<? extends AfflictionEntityEffect> getCodec() {
+    return CODEC;
+  }
+
+  @Override
+  public void apply(ServerWorld world, int stage, Entity target, Vec3d pos) {
+    Random random = target.getRandom();
+    Vec3d movement = target.getMovement();
+    float width = target.getWidth();
+    float heigh = target.getHeight();
+    world.spawnParticles(
+        this.particle,
+        this.horizontalPosition.getPosition(pos.getX(), pos.getX(), width, random),
+        this.verticalPosition.getPosition(pos.getY(), pos.getY() + heigh / 2.0F, heigh, random),
+        this.horizontalPosition.getPosition(pos.getZ(), pos.getZ(), width, random),
+        0,
+        this.horizontalVelocity.getVelocity(movement.getX(), random),
+        this.verticalVelocity.getVelocity(movement.getY(), random),
+        this.horizontalVelocity.getVelocity(movement.getZ(), random),
+        this.speed.get(random)
+    );
+  }
+}
--- src/main/java/syrenyx/distantmoons/initializers/AfflictionEntityEffects.java
+++ src/main/java/syrenyx/distantmoons/initializers/AfflictionEntityEffects.java
@@ -1,15 +1,30 @@
 package syrenyx.distantmoons.initializers;
 
+import com.mojang.serialization.MapCodec;
 import net.minecraft.registry.Registry;
 import syrenyx.distantmoons.UnderDistantMoons;
-import syrenyx.distantmoons.affliction.effect.entity.IgniteEffect;
-import syrenyx.distantmoons.affliction.effect.entity.RunFunctionEffect;
+import syrenyx.distantmoons.affliction.effect.AfflictionEntityEffect;
+import syrenyx.distantmoons.affliction.effect.entity.*;
 
 public class AfflictionEntityEffects {
 
   static {
-    Registry.register(Registries.AFFLICTION_ENTITY_EFFECT_REGISTRY, UnderDistantMoons.identifierOf("ignite"), IgniteEffect.CODEC);
-    Registry.register(Registries.AFFLICTION_ENTITY_EFFECT_REGISTRY, UnderDistantMoons.identifierOf("run_function"), RunFunctionEffect.CODEC);
+    register("all_of", AllOfEntityEffect.CODEC);
+    register("apply_mob_effect", ApplyMobEffectEffect.CODEC);
+    register("damage_entity", DamageEntityEffect.CODEC);
+    register("explode", ExplodeEffect.CODEC);
+    register("ignite", IgniteEffect.CODEC);
+    register("play_sound", PlaySoundEffect.CODEC);
+    register("replace_block", ReplaceBlockEffect.CODEC);
+    register("replace_disk", ReplaceDiskEffect.CODEC);
+    register("run_function", RunFunctionEffect.CODEC);
+    register("set_block_properties", SetBlockPropertiesEffect.CODEC);
+    register("spawn_particles", SpawnParticlesEffect.CODEC);
+    register("summon_entity", SummonEntityEffect.CODEC);
+  }
+
+  private static <T extends AfflictionEntityEffect> void register(String id, MapCodec<T> codec) {
+    Registry.register(Registries.AFFLICTION_ENTITY_EFFECT_REGISTRY, UnderDistantMoons.identifierOf(id), codec);
   }
 
   public static void initialize() {}
